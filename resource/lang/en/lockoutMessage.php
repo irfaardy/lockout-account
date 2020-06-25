46dd83766ab9169d77370b2e19de216f5b6a3e44
IRFA <irfardy2@gmail.com>
2020-06-25 16:25:00 +0700

    LANG

---
+++ resource/lang/en/lockoutMessage.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+	
+	'locked' => 'This account has been locked.',
+	'user_unlock_success' => 'Account Unlocked',
+	'user_lock_success' => 'Account locked',
+	'user_lock_fail' => 'Account fail',
+	'user_lock_404' => "Can't unlock account, because this account not locked",
+
+];
---
+++ resource/lang/id/lockoutMessage.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+	'locked' => 'Akun ini telah dikunci.',
+	'user_unlock_success' => 'Akun telah dibuka.',
+	'user_lock_success' => 'Akun berhasil dikunci.',
+	'user_lock_fail' => 'Akun Gagal Dikunci',
+	'user_lock_404' => "Can't unlock account, because this account not locked",
+
+];
