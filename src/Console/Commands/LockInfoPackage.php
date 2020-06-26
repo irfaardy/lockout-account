3cb9b2aa5ce76ed4a8cf2d70b3444ddde1848fcc
IRFA <irfardy2@gmail.com>
2020-06-26 12:02:10 +0700

    Add Info and commands:

--- src/Console/Commands/LockInfoPackage.php
+++ src/Console/Commands/LockInfoPackage.php
@@ -3,6 +3,7 @@
 namespace Irfa\Lockout\Console\Commands;
 
 use Illuminate\Console\Command;
+use Symfony\Component\Console\Helper\Table;
 
 class LockInfoPackage extends Command
 {
@@ -53,7 +54,19 @@ class LockInfoPackage extends Command
     $this->line('Version 1.0.0 (2020)');
     $this->line('<fg=cyan>https://github.com/irfaardy/lockout-account');
     $conf = config('irfa.lockout');
+
         $this->line('<fg=default>-------------------------------------------------------------------------------');
+         $this->line('<fg=yellow>Commands');
+          $table = new Table($this->output);
+            $table->setRows([
+                ['<fg=cyan>php artisan lockout:lock {username}','<fg=default>locking account'],
+                ['<fg=cyan>php artisan lockout:unlock {username}','<fg=default>unlocking account'],
+                ['<fg=cyan>php artisan lockout:check {username}','<fg=default>checking account with details'],
+                ['<fg=cyan>php artisan lockout:is-locked {username}','<fg=default>checking account (return boolean)'],
+                ['<fg=cyan>php artisan lockout:clear','<fg=default>unlocking all account']]
+            );
+          $table->render();
+         $this->line('<fg=default>-------------------------------------------------------------------------------');
         $this->line('<fg=yellow>Configuration');
     foreach($conf as $key => $val){
         if(is_array($val)){
@@ -63,9 +76,12 @@ class LockInfoPackage extends Command
         } else{
                 $vl = $val;
             }
-
-            $this->line('<fg=default>'.$key.' = <fg=cyan>'.$vl);
+            $row[] = ['<fg=default>'.$key,' <fg=cyan>'.$vl];
         }
+        // dd($row);
+          $table = new Table($this->output);
+            $table->setRows($row);
+                        $table->render();
     }
     
    
